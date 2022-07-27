4e84ea45d0e97641080cc3fc17c1529a708cce3a
lomantech <87063237+lomantech@users.noreply.github.com>
2022-07-27 14:55:09 +0300

    Add app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import CountryDetails from './components/CountryDetails';
+import Error404 from './components/Error404';
+import CountryList from './components/CountryList';
 
 function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<CountryList />} />
+          <Route path="/country/:username" element={<CountryDetails />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
