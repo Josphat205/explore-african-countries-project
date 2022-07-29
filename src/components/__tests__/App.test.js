d341eab79ac4a148137dde7ffef8ecc3f9801de4
lomantech <87063237+lomantech@users.noreply.github.com>
2022-07-29 12:12:43 +0300

    Add app render test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -9,6 +9,6 @@ import '@testing-library/jest-dom/extend-expect';
 describe('app check', () => {
   test('app component', () => {
     render(<Provider store={store}><App /></Provider>);
-    expect(screen.getByTestId('looking-bugs')).toBeInTheDocument();
+    expect(screen.getByTestId('lookingBugs')).toBeInTheDocument();
   });
 });
